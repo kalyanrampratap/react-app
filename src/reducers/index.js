85f11fa3af735470696b88e213ef7fd5752027b7
kalyanrampratap <kalyanrampratap@gmail.com>
2016-12-15 08:46:03 -0800

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 import userListReducer from './reducer_users';
 import activeNav from './reducer_active_nav';
 
-const rootReducer = combineReducers({
+const rootCompReducer = combineReducers({
   userList: userListReducer,
   activeNav
 });
 
-export default rootReducer;
+export default rootCompReducer;
