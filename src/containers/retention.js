112459ba4dde3d6df9c1608878589f45e16c8a3a
kalyanrampratap <kalyanrampratap@gmail.com>
2016-12-15 16:03:42 +0530

    Minor Fixes

--- src/containers/retention.js
+++ src/containers/retention.js
@@ -37,15 +37,10 @@ class Retention extends Component {
 
           <div className="modal-content">
             <div className="modal-header">
-              <button type="button"
-                className="close"
-                data-dismiss="modal"
-                onClick={() => { this.onClickCloseButton() } }>&times;</button>
-
               <h4 className="modal-title">User Details JSON</h4>
             </div>
 
-            <div className="modal-body">
+            <div className="modal-body" style={{ wordWrap: 'break-word' }}>
               <p>{JSON.stringify(data)}</p>
             </div>
 
